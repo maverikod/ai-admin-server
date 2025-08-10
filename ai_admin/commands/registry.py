d0e4a415aa3a76cc7198b6905a2e2561ac3098bc
Vasiliy Zdanovskiy <vasilyvz@gmail.com>
2025-08-10 23:11:06 +0300

    Clear all files from repository

--- ai_admin/commands/registry.py
+++
@@ -1,6 +0,0 @@
-"""Command registry for MCP Empty Server."""
-
-from mcp_proxy_adapter.commands.command_registry import registry
-
-# Re-export the standard registry
-command_registry = registry 
--- ai_admin/version.py
+++
@@ -1,3 +0,0 @@
-"""Version information for AI Admin."""
-
-__version__ = "1.0.0" 
